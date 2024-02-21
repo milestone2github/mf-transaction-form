44b545e70ac67d41af622e55a9f0f96138f7a64a
Kishan Kumar <singhkishan9911@gmail.com>
2024-02-21 11:18:02 +0530

    add: responsiveness to RadioInput

--- Frontend/src/components/common/RadioInput.jsx
+++ Frontend/src/components/common/RadioInput.jsx
@@ -10,9 +10,9 @@ function RadioInput({ options, index, selectedOption, label, name, onChange }) {
   return (
     <div className='flex flex-col text-nowrap'>
       <label className='text-gray-750 text-sm text-left'>{label}</label>
-      <div className='flex gap-2 mt-3'>{
+      <div className='flex flex-col gap-2 mt-3 md:flex-row'>{
         options.map(option => (
-          <div key={option} className='relative'>
+          <div key={option} className='relative text-left py-2'>
             <label
               htmlFor={`${option}-${index}`}
               className={`relative rounded-md cursor-pointer px-4 ps- py-2 ring-inset  ${selectedOption === option ? 'ring-light-blue ring-2 bg-light-blue/10 text-light-blue' : 'bg-transparent ring-light-gray ring-1'} before:rounded-md before:absolute before:-inset-0 before:bg-primary-white before:-z-10`}
